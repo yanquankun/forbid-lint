99d8a6928fec5ad9b5d1b8d67e2eab1ca8911484
yanquankun <yanquankun@58.com>
2025-02-11 14:56:04 +0800

    update: publishCI.ts

--- scripts/publishCI.ts
+++ scripts/publishCI.ts
@@ -1,14 +1,10 @@
 import { publish } from "./publish";
 import path from "path";
 
-// Check the tag passed in CI, and skip provenance if tag has `@` due to
-// https://github.com/slsa-framework/slsa-github-generator/pull/2758 not released
-const tag = process.argv.slice(2)[0] ?? "";
-const provenance = !tag.includes("@");
-
 publish({
   defaultPackage: "@vexjs/forbid-lint",
   getPkgDir: () => path.resolve(__dirname, "../"),
-  provenance,
+  // remove security check
+  provenance: false,
   packageManager: "pnpm",
 });
